fffb98f4269da60ad76d95c284297b1186e67810
Paris Vigay <parisvigay@pariss-mbp.home>
2025-09-17 12:21:13 +0100

    Backend updates for Neon DB

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l234v2ww$l%+z8p##2u+=!m*4(z&!jkpfxadx#i2$+2@o9olpq'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=env('DATABASE_URL')
     )
 }
 
